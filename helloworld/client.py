37e4a0c2c60620930e9f2279894d30fe70775ec9
tanbo <tanbo0228@thundersoft.com>
2019-07-03 14:56:24 +0800

    learn get and post

--- helloworld/client.py
+++ helloworld/client.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3.5
 # -*- coding:utf-8 -*-
 
+import requests
+
+user_info = {'name': 'letian', 'password': '123'}
+r = requests.post("http://127.0.0.1:8080/register", data=user_info)
+print(r.text)
+
+json_data = {'a': 1, 'b': 2}
+r = requests.post("http://127.0.0.1:8080/add", json=json_data)
+print(r.text)
--- helloworld/my_server.py
+++ helloworld/my_server.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3.5
 # -*- coding:utf-8 -*-
 
-from flask import Flask ,request
-
+from flask import Flask, request
 
 app = Flask(__name__)
-print(Flask.__doc__)
 
+#get请求，通过http://127.0.0.1:8080/?user=Flask&time&p=7&p=8访问
 @app.route('/')
-def helloworld():
-    print(request.path)
-    print(request.full_path)
-    return "helloworld!"
-    pass
+def hello_world():
+    r = request.args.getlist('p')
+    print(type(r),type(r[0]))
+    if r == None:
+        return ' '
+    return str(int(r[0])+int(r[1]))
+
+#post请求,通过client.py访问
+@app.route('/register', methods=['POST'])
+def register():
+    print(request.headers)
+    print(request.stream.read())
+    return 'welcome'
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    print(request.headers)
+    print(type(request.json))
+    print(request.json)
+    result = request.json['a'] + request.json['b']
+    return str(result)
+
+
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=8080,debug=True)
