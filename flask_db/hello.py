3ffd17ae55813194c00f3f9a2eefb8c696d3e6e1
DESKTOP-7QQ8MAB\Administrator <1042426411@qq.com>
2020-03-30 23:30:31 +0800

    Command line options with flash_script

--- flask_db/hello.py
+++ flask_db/hello.py
@@ -2,8 +2,11 @@
 #--*-- coding=utf-8--*--
 
 from flask import Flask
+from  flask_script import Manager
+
 app = Flask(__name__)
 
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -14,4 +17,4 @@ def user(name):
     return '<h1>hello %s !</h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
