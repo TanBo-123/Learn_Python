149b4460d7dbcafe95c4aacfe3aa6e2956d9ad0f
DESKTOP-7QQ8MAB\Administrator <1042426411@qq.com>
2020-03-30 23:02:43 +0800

    1a

--- flask_db/hello.py
+++ flask_db/hello.py
@@ -1,26 +1,13 @@
 #!/user/bin/env python3.5
 #--*-- coding=utf-8--*--
 
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-
-#初始化
+from flask import Flask
 app = Flask(__name__)
-#初始化 Flask-Bootstrap
-bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+    return '<h1>Hello World!</h1>'
 
 
 if __name__ == '__main__':
