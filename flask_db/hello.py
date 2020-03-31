f19228d627cbe5d2d183a85e72a896cc78a96f86
DESKTOP-7QQ8MAB\Administrator <1042426411@qq.com>
2020-03-31 22:17:09 +0800

    Templaters with Flask_Bootstrap

--- flask_db/hello.py
+++ flask_db/hello.py
@@ -2,11 +2,10 @@
 #--*-- coding=utf-8--*--
 
 from flask import Flask,render_template
-from  flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
-manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
