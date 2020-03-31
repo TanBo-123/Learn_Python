a70c05ee817cfb9abac2a2fba88ed316494fafb6
DESKTOP-7QQ8MAB\Administrator <1042426411@qq.com>
2020-04-01 00:01:40 +0800

    Date and time  with Flask_Moment(failed)

--- flask_db/hello.py
+++ flask_db/hello.py
@@ -1,11 +1,16 @@
 #!/user/bin/env python3.5
-#--*-- coding=utf-8--*--
-
-from flask import Flask,render_template
+# --*-- coding=utf-8--*--
+from datetime import datetime
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -16,13 +21,17 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html', name=name)
+
 
 if __name__ == '__main__':
     app.run()
